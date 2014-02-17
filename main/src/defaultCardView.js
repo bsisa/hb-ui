42253dded45ec85a1240c1c3ba1f07227ad7b25a
guy <guy@gadp.ch>
2014-02-17 16:02:18 +0100

    Latest status

--- main/src/defaultCardView.js
+++ main/src/defaultCardView.js
@@ -1,5 +1,16 @@
 (function() {
 
+    var UploadFileCtrl = function ($scope, $modalInstance, $$renvoi) {
+
+        $scope.$$renvoi = $$renvoi;
+
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
+
+
+    };
+
     angular.module('hb5').controller('DefaultCardController', ['$scope', 'GeoxmlService', '$modal', '$routeParams', function($scope, GeoxmlService, $modal, $routeParams) {
 
 
@@ -13,7 +24,57 @@
             ACTIVITE: "Activité",
             PERSONNE: "Personne",
             DOCUMENT: "Document"
+        };
+
+        $scope.typeTypes = {
+            GEOGRAPHIE: "Géographie",
+            SCHEMATIQUE: "Schématique"
+        };
+
+        $scope.dimensionNames = {
+            LONGUEUR: "Longueur",
+            SURFACE: "Surface",
+            RAYON: "Rayon"
+        };
+
+        $scope.formFunctions = {
+            LIBRE: "Libre",
+            LIE: "Lié",
+            BASE: "Base"
+        };
+
+        $scope.lineDirections = {
+            AVAL: "Aval",
+            AMONT: "Amont"
+        };
+
+        $scope.lineFunctions = {
+            FRONTIERE: "Frontière",
+            AXE: "Axe"
+        };
+
+        $scope.addRow = function(elfin, path, rowObject) {
+            GeoxmlService.addRow(elfin, path, rowObject);
         }
+
+        $scope.uploadFile = function (renvoi) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'defaultUploadFile.html',
+                controller: UploadFileCtrl,
+                resolve: {
+                    $$renvoi: renvoi
+                },
+                backdrop: 'static'
+            });
+
+            modalInstance.result.then(function (selection) {
+                $scope.$$activeConfiguration = selection;
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
+
 
         if (GeoxmlService.validateId($scope.collectionId) && GeoxmlService.validateId($scope.elfinId)) {
             GeoxmlService.getElfin($scope.collectionId, $scope.elfinId).get()
