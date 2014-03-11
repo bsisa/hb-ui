ad93d3bb69d7749a6604bc6cdc3c329c740889f6
refond <refon@pobox.com>
2014-03-11 14:17:12 +0100

    Added small sharedMessages service to exchange messages data between miscellaneous controllers. For instance when deleting a card the success message must be displayed on the redirect to page within redirect page controller scope.

--- main/src/hb5.js
+++ main/src/hb5.js
@@ -2,14 +2,8 @@
  * Created by guy on 19.01.14.
  */
 
-                        
-//            .when('/elfin/:collectionId/:classe', {
-//                templateUrl: 'views/'+ $routeParams.classe +'.html',
-//                controller: 'DefaultListsController'
-//            })
-
 (function() {
-	// TODO: solve functions naming clash between 'mgcrea.ngStrap' and 'localytics.directives' on $modal
+	// TODO: solve functions naming clash between 'mgcrea.ngStrap' and 'ui.bootstrap' on $modal
 	//['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']
 	//  var hb5 = angular.module('hb5', ['geoxml', 'ngRoute', 'ui.bootstrap', 'localytics.directives']);	
     var hb5 = angular.module('hb5', ['ngAnimate', 'ngSanitize', 'geoxml', 'ngRoute', 'ui.bootstrap', 'localytics.directives']);
@@ -36,6 +30,10 @@
     }]);
     
 
+    /**
+     * Basic filter to add an "s" to result titles 
+     * depending on the number of entries returned. 
+     */
 	hb5.filter('plural', function() {
 		return function(number) {
 			if (number > 1) {
@@ -45,7 +43,29 @@
 			}
 		};
 	});
-    
+	
+	
+	/**
+	 * Small service used to exchange messages data between miscellaneous controllers.
+	 * For instance when deleting a card the success message must be displayed on the
+	 * redirect to page within redirect page controller scope.
+	 */
+	hb5.service('sharedMessages', function () {
+        var statusMessage = {
+            data: "First"
+        };
+
+        return {
+            getStatusMessage:function () {
+                return statusMessage;
+            },
+            setStatusMessage:function (value) {
+            	statusMessage.data = value;
+            }
+        };
+    });
+
+   
 })();
 
 
