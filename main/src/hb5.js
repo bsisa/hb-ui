a6b907a357c46bbb395e8756c74d0c106c554d2c
refond <refon@pobox.com>
2014-05-05 11:59:27 +0200

    Created new hbCardContainer (hb-card-container) directive to enable HTML template code reuse for each specific HB card tailored to a given geoxml object CLASSE. Reuse of controller logic is not yet included but could as future improvement.

--- main/src/hb5.js
+++ main/src/hb5.js
@@ -192,6 +192,24 @@
         };
     });	
 
+	
+	
+    /**
+     * Directive allowing card buttons group and head properties layout reuse (template).
+     * Note: TODO: manage controller reusable logic to be included with a parent scope 
+     * to more specific logic of the transcluded element logic.
+     * 
+     */
+    angular.module('hb5').directive('hbCardContainer', function () {
+
+		return {
+		    restrict: 'A',
+		    transclude: true,
+			templateUrl : "/assets/views/hbCardContainer.html"
+		};
+	
+    });	
+	
    
 })();
 
