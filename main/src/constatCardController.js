af881107f7e0ce2d8778fcaecfc998683b4fc28e
refond <refon@pobox.com>
2014-05-13 16:19:51 +0200

    ACTOR selection modal first draft (not functional yet, framework only).

--- main/src/constatCardController.js
+++ main/src/constatCardController.js
@@ -21,6 +21,59 @@
 							SUIVRE : "SUIVRE"
 						};
 
+						
+// ===
+		
+				        /**
+				         * Modal panel to select an actor.
+				         */
+				        $scope.chooseActor = function (itemDefinition) {
+				        	
+				            var modalInstance = $modal.open({
+				                templateUrl: '/assets/views/chooseActor.html',
+				                scope: $scope,
+				                controller: 'ChooseActorCtroller',
+				                resolve: {
+				                	itemDefinition: function () {
+				                    	return itemDefinition;
+				                    }               
+				                },                
+				                backdrop: 'static'
+				            });
+
+				            /**
+				             * Process modalInstance.close action
+				             */
+				            modalInstance.result.then(function (result) {
+				                var queryString = hbUtil.buildUrlQueryString(result.parameters);
+				            	var urlWithQuery = result.url + queryString;
+				            	if (result.newWindow && result.newWindow === 'true') {
+				            		$window.open(urlWithQuery);
+				            	} else {
+				            		$location.path(urlWithQuery); 
+				            	}
+				            }, function () {
+				                console.log('Choose params modal dismissed at: ' + new Date());
+				            });
+				        };						
+						
+// ===
+						
+						
+						
+						
 					} ]);
+	
+	
+    angular.module('hb5').controller('ChooseActorCtroller', ['$scope', '$modalInstance', 'itemDefinition', function($scope, $modalInstance, itemDefinition) {
+    	$scope.itemDefinition = itemDefinition;
+        $scope.ok = function () {
+            $modalInstance.close($scope.itemDefinition);
+        };
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }]);	
+	
 
 })();
