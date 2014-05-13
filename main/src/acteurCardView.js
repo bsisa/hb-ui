c6fb8d588fdac822349b4f8952c5312f7cc5aa53
refond <refon@pobox.com>
2014-05-13 14:21:50 +0200

    Formatting + Added TODO.

--- main/src/acteurCardView.js
+++ main/src/acteurCardView.js
@@ -1,19 +1,29 @@
 (function() {
 
-    angular.module('hb5').controller('ActeurCardController', ['$scope', 'GeoxmlService', '$modal', '$routeParams', '$location', 'hbAlertMessages', 'hbUtil', function($scope, GeoxmlService, $modal, $routeParams, $location, hbAlertMessages, hbUtil) {
-    
-    	console.log("    >>>> Using ActeurCardController (slim)");
-    	
-        $scope.qualiteList = {
-        		"Gérant":"Gérant",
-        		"Concierge":"Concierge",
-        		"Responsable chauffage":"Responsable chauffage",
-        		"Entreprise":"Entreprise",
-        		"Collaborateur": "Collaborateur"        		
-        }; 	
-    	
-        
-    }]);
+	angular.module('hb5').controller(
+			'ActeurCardController',
+			[
+					'$scope',
+					'GeoxmlService',
+					'$modal',
+					'$routeParams',
+					'$location',
+					'hbAlertMessages',
+					'hbUtil',
+					function($scope, GeoxmlService, $modal, $routeParams,
+							$location, hbAlertMessages, hbUtil) {
 
+						console.log("    >>>> Using ActeurCardController");
+
+						// TODO: get this dynamically from HB5 catalogue
+						$scope.qualiteList = {
+							"Gérant" : "Gérant",
+							"Concierge" : "Concierge",
+							"Responsable chauffage" : "Responsable chauffage",
+							"Entreprise" : "Entreprise",
+							"Collaborateur" : "Collaborateur"
+						};
+
+					} ]);
 
 })();
