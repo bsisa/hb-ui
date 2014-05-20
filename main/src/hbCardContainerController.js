a9526c8fc911e22b020b66205ce6cbd18600a1eb
refond <refon@pobox.com>
2014-05-20 19:45:48 +0200

    Moving from console to $log for logging. Code clean up.

--- main/src/hbCardContainerController.js
+++ main/src/hbCardContainerController.js
@@ -21,10 +21,13 @@
         
     };    
         
-    // TODO: Rename to HbCardContainerController
-    angular.module('hb5').controller('HbCardContainerController', ['$scope', 'GeoxmlService', '$modal', '$routeParams', '$location', 'hbAlertMessages', 'hbUtil', function($scope, GeoxmlService, $modal, $routeParams, $location, hbAlertMessages, hbUtil) {
+    /**
+     * Controller for hb-card-container directive providing the global elfin card toolbox and 
+     * insert point (ng-transclude) for custom cards layout. 
+     */
+    angular.module('hb5').controller('HbCardContainerController', ['$scope', 'GeoxmlService', '$modal', '$routeParams', '$location', '$log', 'hbAlertMessages', 'hbUtil', function($scope, GeoxmlService, $modal, $routeParams, $location, $log, hbAlertMessages, hbUtil) {
     
-    	console.log("HbCardContainerController called at " + new Date());
+    	$log.debug("HbCardContainerController called at " + new Date());
     	
     	// ====================================================================
     	// Global logic to move to hb-card-container directive controller.
@@ -37,37 +40,24 @@
         // The ELFIN to be edited once obtained from REST API.
         $scope.elfin = null;
         
-        // TODO: check if listening to model change from controllers can be improved.
-//        $scope.$watch('elfin', function(newValue, oldValue) {
-//			// Only change form status if elfin was initialised.
-//        	if (oldValue) {
-//				$scope.elfinForm.$setDirty();
-//				//Reminder: to restore form status to pristine.
-//				//$scope.elfinForm.$setPristine();
-//			}
-//        }, true);
-        
     	$scope.removeKeyword = function ( index ) {
-    		console.log("removing MOCLE at index " + index);
+    		$log.debug("removing MOCLE at index " + index);
     	    $scope.elfin.IDENTIFIANT.MOTCLE.splice(index,1);
     	};
     	
     	// Wrapper for ELFIN PUT (update) operation
         $scope.putElfin = function (elfin) {
-        	console.log("HbCardContainerController putElfin called at " + new Date());
+        	$log.debug("HbCardContainerController putElfin called at " + new Date());
        		elfin.put().then( 
        			function() { 
-       				console.log("All ok");
-       				// Consider good practice to reload the actual db  
+       				$log.debug("All ok");
+       				// Considered good practice to reload the actual db  
        				// content unless server load is a concern.
        				$scope.getElfin($scope.collectionId,$scope.elfinId);
        				$scope.elfinForm.$setPristine();
-       				// Message feedback is removed in favour of dirty/pristine CSS 
-       				//var message = "Mise à jour effectuée avec succès.";
-       				//hbAlertMessages.addAlert("success",message);
        			}, 
        			function(response) { 
-       				console.log("Error with status code", response.status);
+       				$log.debug("Error with status code", response.status);
        				var message = "La mise à jour a échoué (statut de retour: "+ response.status+ ")";
 					hbAlertMessages.addAlert("danger",message);
        			} 
@@ -77,7 +67,7 @@
     	// Wrapper for ELFIN DELETE operation
         $scope.delElfin = function (elfin) {
         	
-        	console.log("HbCardContainerController delElfin called at " + new Date());
+        	$log.debug("HbCardContainerController delElfin called at " + new Date());
         	
         	var modalInstance = $modal.open({
                 templateUrl: 'deleteConfirmModalPanel.html',
@@ -96,7 +86,7 @@
                			function(response) { 
                				var message = "La suppression a échoué. Veuillez s.v.p. recommencer. Si le problème persiste contactez votre administrateur système et lui communiquer le message suivant: " + response.status;
                				hbAlertMessages.addAlert("danger",message);
-               				console.log("Error: ELFIN delete failure with status code", response.status);
+               				$log.debug("Error: ELFIN delete failure with status code", response.status);
                			} 
                		);
             }, function () {
@@ -157,14 +147,14 @@
             modalInstance.result.then(function (selection) {
                 $scope.$$activeConfiguration = selection;
             }, function () {
-                console.log('Modal dismissed at: ' + new Date());
+                $log.debug('Modal dismissed at: ' + new Date());
             });
         };
 
         
         $scope.getElfin = function (collectionId, elfinId) {
         	
-        	console.log("HbCardContainerController getElfin called at " + new Date());
+        	$log.debug("HbCardContainerController getElfin called at " + new Date());
         	
         	if (GeoxmlService.validateId(collectionId) && GeoxmlService.validateId(elfinId)) {
 		        GeoxmlService.getElfin(collectionId, elfinId).get()
@@ -174,10 +164,10 @@
 		        	// DONE: Safe array ordering is mandatory to prevent null accessor related exception
 		        	//       Need review of other similar operations
 		        	if ( elfin['CARACTERISTIQUE'] != null && elfin['CARACTERISTIQUE']['CARSET'] != null && elfin['CARACTERISTIQUE']['CARSET']['CAR'] != null) {
-		        		console.log("Found CARSET/CAR...");
+		        		$log.debug("Found CARSET/CAR...");
 		        		hbUtil.reorderArrayByPOS(elfin['CARACTERISTIQUE']['CARSET']['CAR']);
 		        	} else {
-		        		console.log("No CARSET/CAR...");
+		        		$log.debug("No CARSET/CAR...");
 		        	}
 		            $scope.elfin = elfin;
 		        }, function(response) {
@@ -192,10 +182,7 @@
         };
 
         $scope.getElfin($scope.collectionId,$scope.elfinId);
-        
-    	// ====================================================================
-    	// END OF Global logic to move to hb-card-container directive controller.
-    	// ====================================================================        
+                
         
     }]);
 
