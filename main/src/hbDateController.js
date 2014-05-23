1f754ee48620cc0ad7799cee3b05e05aee8d2676
refond <refon@pobox.com>
2014-05-23 11:42:43 +0200

    hb-date directive updates with datepicker-popup. Need fix manual entry.

--- main/src/hbDateController.js
+++ main/src/hbDateController.js
@@ -1,32 +1,92 @@
 (function() {
 
-	
 	angular.module('hb5').controller(
 			'HbDateController',
 			[
 					'$scope',
 					'$log',
+					'$filter',
 					'hbUtil',
-					function($scope, $log, hbUtil) {
+					function($scope, $log, $filter, hbUtil) {
 
+						// Initialisation
+						$scope.hbDateCtrl = null;
+
+						this.$setHbDateCtrl = function(ctrl) {
+							$log.debug("    >>>> $setHbDateCtrl <<<< ");
+							$scope.hbDateCtrl = ctrl;
+							// Specify how UI should be updated
+							$scope.hbDateCtrl.$viewChangeListeners.push( function() {
+								$log.debug("$viewChangeListeners called for hbDateCtrl: ");
+								$log.debug("hbDateCtrl.$modelValue = " + $scope.hbDateCtrl.$modelValue);
+								$log.debug("hbDateCtrl.$viewValue = " + $scope.hbDateCtrl.$viewValue);
+					        });
+							$scope.hbDateCtrl.$render( function() {
+								$log.debug("$render called for hbDateCtrl: ");
+								$log.debug("hbDateCtrl.$modelValue = " + $scope.hbDateCtrl.$modelValue);
+								$log.debug("hbDateCtrl.$viewValue = " + $scope.hbDateCtrl.$viewValue);
+					        });							
+						};
+						
 						$log.debug("    >>>> Using HbDateController");	
 	
 						// TODO: make use of this 
-						$scope.geoXmlDateFormat = "yyyy-mm-dd";
+						var GEOXML_DATE_FORMAT = "yyyy-MM-dd";
 						
 						// Quick and dirty test
-						var splittedModel = $scope.hbDateModel.split("-");
-						$scope.date = new Date(splittedModel[2], splittedModel[1] - 1, splittedModel[0]);
-						
-						$log.debug("hbDateModel is : " + $scope.hbDateModel + ", date is : " + $scope.date);
-						
-						// Update hbDateModel on date change
+//						var splittedModel = $scope.$modelValue.split("-");
+//						$scope.date = new Date(splittedModel[2], splittedModel[1] - 1, splittedModel[0]);
+						$scope.date = null;
+//						
+//						$log.debug("$modelValue is : " + $scope.$modelValue + ", date is : " + $scope.date);
+//						
+						// Update ngModel on date change
 						$scope.$watch('date', function (newDate, oldDate) {
-							$scope.hbDateModel = ($scope.date.getFullYear() + '-' + $scope.date.getMonth() + 1) + '-' + $scope.date.getDate();
-							$log.debug("hbDateModel updated to : " + $scope.hbDateModel + ", date is : " + $scope.date);
+							$log.debug("Date change from " + oldDate + " to " + newDate);
+							if (newDate) {
+								var isoTextDate = $filter('date')($scope.date, GEOXML_DATE_FORMAT);
+								$log.debug("isoTextDate: " + isoTextDate);
+								// Simulate user entry...
+								$log.debug("Simulate user entry... ");
+								$scope.hbDateCtrl.$viewValue = isoTextDate;
+								$log.debug("$commitViewValue ... ");
+								$scope.hbDateCtrl.$commitViewValue();
+//
+//								$log.debug("Update the model ... ");
+//								$scope.hbDateCtrl.$modelValue = isoTextDate;
+							}
+							if ($scope.hbDateCtrl) {
+								$log.debug("hbDateCtrl.$modelValue = " + $scope.hbDateCtrl.$modelValue);
+								$log.debug("hbDateCtrl.$viewValue = " + $scope.hbDateCtrl.$viewValue);
+							}							
+//							$scope.$modelValue = ($scope.date.getFullYear() + '-' + $scope.date.getMonth() + 1) + '-' + $scope.date.getDate();
+//							$log.debug("$modelValue updated to : " + $scope.$modelValue + ", date is : " + $scope.date);
 						}, true);
  
-
+						// Listen to hbDateCtrl model value change. (reference to method is safe thanks angular.noop)
+						$scope.$watch('hbDateCtrl.$modelValue', function (newValue, oldValue) {
+							
+							$log.debug("hbDateCtrl.$modelValue changed : " + oldValue + " => " + newValue);
+							
+							if ($scope.hbDateCtrl) {
+								// update datepicker date with hbDate
+								var milliseconds = Date.parse($scope.hbDateCtrl.$modelValue);
+					            if (!isNaN(milliseconds)) {
+					            	$scope.date = new Date(milliseconds);
+					            } else {
+					            	if ($scope.myWidget) {
+					            		$log.debug("Found myWidget setting it to invalid...");
+					            		var validationErrorKey = "hbDateInvalid";
+					            		var isValid = false;
+					            		$scope.myWidget.$setValidity(validationErrorKey, isValid);
+					            	} else {
+					            		$log.debug("Could not find myWidget in scope");
+					            	}
+					            }
+							}							
+						}, true);						
+						
+						
 						// TODO: should be automatic with $locale providing the correct id i.e.: fr-ch, de-ch,...
 						$scope.dateFormat = 'dd.MM.yyyy';						
 						
@@ -46,10 +106,10 @@
 						// ============================================
 						// TODO: move to hbDate directive - END
 						// ============================================
-
-
+	
 					} ]);
 	
+
 
         
 })();
--- main/src/hbDateDirective.js
+++ main/src/hbDateDirective.js
@@ -9,17 +9,43 @@
     angular.module('hb5').directive('hbDate', function () {
 
     	return {
-		    require: '^hbCardContainer',
+		    require: ['hbDate', 'ngModel'],
 			restrict: 'A',
+			priority: 1,
 			templateUrl : "/assets/views/hbDate.html",
 			controller: 'HbDateController',
-			replace : true,
-			scope : {
-				'hbDateModel' : '='  
+			link: function ($scope, $element, $attrs, ctrls) {
+				var hbDateCtrl = ctrls[0];
+				var ngModelCtrl = ctrls[1];
+				
+				console.log("    >>>> hbDate link function !!! <<<<");
+				
+//				console.log("    >>>> ngModelCtrl.$modelValue = " + ngModelCtrl.$modelValue);
+//				console.log("    >>>> ngModelCtrl.$viewValue = " + ngModelCtrl.$viewValue);
+
+				console.log("    >>>> About to link ngModelCtrl to hbDateCtrl...");
+				// Pass hb-date ng-model sibling definition controller reference to hb-date controller. 
+				hbDateCtrl.$setHbDateCtrl(ngModelCtrl);
+				
+				
+				
+				//$scope.dateModel = ngModel;
 			}
 		};
 	
     });
 
+//		    require: '^hbCardContainer',
+//	replace : true,
+//	scope : {
+//		'hbDateModel' : '='      
+//	}    
+    
+    /*
+,
+			link: function ($scope, $element, $attrs, ngModel) {
+				$scope.dateModel = ngModel;
+			}
+     */
 
 })();
