ea72a51b5cc7bfcafc3ec464a6330ee5dd478307
refond <refon@pobox.com>
2014-06-18 01:47:09 +0200

    Pass xpath parameter through restangular to hb-api.

--- main/src/hbListContainerController.js
+++ main/src/hbListContainerController.js
@@ -4,6 +4,13 @@
     
     	$log.debug("    >>>> HbListContainerController called...");
     	
+    	if ($routeParams.xpath) {
+    		$log.debug("    >>>> XPATH parameter found:  " + $routeParams.xpath);	
+    	} else {
+    		$log.debug("    >>>> NO XPATH parameter found...");
+    	}
+    	
+    	
     	// Collection id parameter obtained from hb5.config $routeProvider
         $scope.collectionId = $routeParams.collectionId;
         // Contains a JSON Array of ELFINs resulting of the GeoxmlService query   
@@ -11,7 +18,8 @@
         $scope.elfinsCount = null;
         
         if (GeoxmlService.validateId($scope.collectionId)) {
-            GeoxmlService.getCollection($scope.collectionId).getList()
+        	if ($routeParams.xpath) {
+                GeoxmlService.getCollection($scope.collectionId).getList({"xpath" : $routeParams.xpath})
                 .then(function(elfins) {
                 	if (elfins == null) {
                 		$scope.elfins = elfins;
@@ -23,7 +31,22 @@
                 }, function(response) {
                     var message = "Le chargement des informations a échoué (statut de retour: " + response.status + ")";
                     hbAlertMessages.addAlert("danger",message);
-                });
+                });        		
+        	} else {
+                GeoxmlService.getCollection($scope.collectionId).getList()
+                .then(function(elfins) {
+                	if (elfins == null) {
+                		$scope.elfins = elfins;
+                		$scope.elfinsCount = 0;
+                	} else {
+                		$scope.elfins = elfins;
+                		$scope.elfinsCount = elfins.length;
+                	}
+                }, function(response) {
+                    var message = "Le chargement des informations a échoué (statut de retour: " + response.status + ")";
+                    hbAlertMessages.addAlert("danger",message);
+                });	
+        	}
         } else {
             var message = "L'identifiants de collection (" + $scope.collectionId + " ) n'est pas correct.";
             hbAlertMessages.addAlert("warning",message);
