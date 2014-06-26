0672ad3560522c4db723bc50e651c0284ec4fa16
refond <refon@pobox.com>
2014-06-26 17:25:49 +0200

    Add new hb-immeuble-list directive to allow IMMEUBLE list specific behaviour in HbImmeubleListController (search, sort,...)

---
+++ main/src/hbImmeubleListController.js
@@ -0,0 +1,12 @@
+(function() {
+
+    angular.module('hb5').controller('HbImmeubleListController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, hbAlertMessages, hbUtil) {
+    
+    	$log.debug("    >>>> HbImmeubleListController called...");
+    	
+
+    }]);
+
+
+})();
+
