e7337353763dce4377177545ef49c1929b33d030
refond <refon@pobox.com>
2014-06-26 17:39:58 +0200

    Implemented client side per field order by.

--- main/src/hbImmeubleListController.js
+++ main/src/hbImmeubleListController.js
@@ -4,6 +4,9 @@
     
     	$log.debug("    >>>> HbImmeubleListController called...");
     	
+    	// Default order is by "Building management" 
+    	$scope.predicate = 'IDENTIFIANT.OBJECTIF';
+    	$scope.reverse = false;
 
     }]);
 
