c6af9dfea2efc553fdcfadef02ea9297fa424cab
refond <refon@pobox.com>
2014-07-02 13:04:19 +0200

    Get available ACTOR roles dynamically from HB5 catalogue.

--- main/src/hbActeurCardController.js
+++ main/src/hbActeurCardController.js
@@ -16,14 +16,26 @@
 
 						$log.debug("    >>>> Using HbActeurCardController ");
 						
-						// TODO: get this dynamically from HB5 catalogue
-						$scope.qualiteList = {
-							"Gérant" : "Gérant",
-							"Concierge" : "Concierge",
-							"Responsable chauffage" : "Responsable chauffage",
-							"Entreprise" : "Entreprise",
-							"Collaborateur" : "Collaborateur"
-						};						
+						// Get available roles dynamically from HB5 catalogue
+				    	$scope.$watch('elfin.Id', function() { 
+
+				    		if ($scope.elfin!=null) {
+
+					            // Asychronous ACTEUR template preloading
+					            GeoxmlService.getNewElfin("ACTEUR").get()
+					            .then(function(actor) {
+					            		// Get actor roles from catalogue default
+					            		$scope.actorRoles = hbUtil.buildArrayFromCatalogueDefault(actor.IDENTIFIANT.QUALITE);
+									},
+									function(response) {
+										var message = "Les valeurs par défaut pour la CLASSE ACTEUR n'ont pas pu être chargées. (statut de retour: "+ response.status+ ")";
+										hbAlertMessages.addAlert("danger",message);
+									});				    			
+				    		};
+				    		
+				    	}, true);						
+						
+						
 						
 					} ]);
 	
