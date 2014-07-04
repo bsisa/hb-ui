f5fbbd376b5458e951be172db6a3696532783230
refond <refon@pobox.com>
2014-07-04 17:45:34 +0200

    New hb-transaction-create-card directive created. We need a separate create workflow than the edit one.

---
+++ main/src/hbTransactionCreateCardDirective.js
@@ -0,0 +1,20 @@
+(function() {
+
+
+    /**
+     * Directive encapsulating GeoXML ELFIN[@CLASSE='TRANSCATION'] specific view 
+     * and behaviour for CREATION only. Editing, reading will be done with another 
+     * view.
+     */
+    angular.module('hb5').directive('hbTransactionCreateCard', function () {
+
+    	return {
+		    require: '^hbCardContainer',
+			restrict: 'A',
+		    templateUrl : "/assets/views/hbTransactionCreateCard.html",
+			controller: 'HbTransactionCardController'
+		};
+	
+    });
+
+})();
