9f1892dd469b7ec46c81c417145ec20d25d1ecfe
refond <refon@pobox.com>
2014-07-06 15:14:52 +0200

    Defined relevant default "order by" creteria. Code clean up.

--- main/src/hbConstatListController.js
+++ main/src/hbConstatListController.js
@@ -4,32 +4,9 @@
     
     	$log.debug("    >>>> HbConstatListController called... " );
 
-    	// Default order is by "Building management" 
-    	$scope.predicate = 'IDENTIFIANT.OBJECTIF';
+    	// Default order is by "Groupe" 
+    	$scope.predicate = 'GROUPE';
     	$scope.reverse = false;
-
-    	// Object holding user entered search (filter) criteria 
-    	// SPECIFIC to IMMEUBLE...
-//    	$scope.search = {
-//    			"owner" : "",
-//    			"registerNb" : "",
-//    			"place" : "",
-//    			"buildingNb" : "",
-//    			"address" : ""
-//    	};    	
-    	
-//        $scope.$watch('elfins', function(newElfins, oldElfins) {
-//        	if (newElfins) {
-//        		// Note: underscore syntax is provided by underscore.js library.
-//        		var uniqueRoles = _.chain($scope.elfins).pluck('IDENTIFIANT').pluck('QUALITE').unique().value(); 
-//        		$scope.isCollaboratorOnly = (uniqueRoles.length == 1 && uniqueRoles[0] == 'Collaborateur');
-//        		$scope.isConciergeOnly = (uniqueRoles.length == 1 && uniqueRoles[0] == 'Concierge');
-//        		$scope.isEntrepriseOnly = (uniqueRoles.length == 1 && uniqueRoles[0] == 'Entreprise');
-//        		$scope.isRespChauffageOnly = (uniqueRoles.length == 1 && uniqueRoles[0] == 'Responsable chauffage');
-//        		$scope.isManagerOnly = (uniqueRoles.length == 1 && uniqueRoles[0] == 'Gérant');
-//        		$scope.isMany = (uniqueRoles.length > 1 || uniqueRoles.length < 1);
-//        	}
-//        });
         
     }]);
 
