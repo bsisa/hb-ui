bcc8c18a93380b3d69818357ee62285dee5aabe0
refond <refon@pobox.com>
2014-07-07 09:45:23 +0200

    hb-annexes-component directive implemented. Possible improvement: encapsulate API request within a function to manage {"ERROR":"annex.file.not.found","DESCRIPTION":""} exceptions with end user friendly message using hbAlertService.

--- main/src/hbAnnexesComponentController.js
+++ main/src/hbAnnexesComponentController.js
@@ -7,11 +7,38 @@
 					[
 							'$scope',
 							'$log',
-							function($scope, $log) {
+							'hbUtil',
+							function($scope, $log, hbUtil) {
     
 									$log.debug("    >>>> Using HbAnnexesComponentController");
 							        
-									$scope.annexes = null;
+									$scope.annexes = [];
+									
+									$scope.$watch('elfin.Id', function() { 
+
+							    		if ($scope.elfin!=null) {
+
+							    			// Get annexes
+								            if ($scope.elfin.ANNEXE) {
+									            for (var i = 0; i < $scope.elfin.ANNEXE.RENVOI.length; i++) {
+													
+									            	var currentRenvoi = $scope.elfin.ANNEXE.RENVOI[i];
+													
+									            	// Skip photo(s)
+													if ( currentRenvoi.VALUE.toLowerCase().indexOf("photo") == -1 ) {
+														// Photo found build link
+														var currentLink = currentRenvoi;
+														var linkApiUrl = hbUtil.getLinkFileApiUrl($scope.elfin.ID_G, $scope.elfin.Id, currentLink.LIEN);
+														var linkFileName = hbUtil.getLinkFileName(currentLink.LIEN);
+														$scope.annexes.push({link: linkApiUrl, fileName: linkFileName});
+													}
+												}
+								            }				
+							    		};
+							    		
+							    	}, true);
+									
+									
 									        
 							    } ]);
 
