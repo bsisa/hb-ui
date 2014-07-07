218238d32802a40254d5f6b908f2a9a259f82190
refond <refon@pobox.com>
2014-07-07 19:34:05 +0200

    Implemented getPrestation.

--- main/src/hbTransactionCardController.js
+++ main/src/hbTransactionCardController.js
@@ -25,6 +25,8 @@
 
 								$log.debug("    >>>> Using HbTransactionCardController");
 
+								$scope.constatSelectionHelperSai = '';
+								
 								// Benefit from server side cache...
 								var xpathForImmeubles = "//ELFIN[@CLASSE='IMMEUBLE']";
 								// TODO: immeublesCollectionId must come from
@@ -80,6 +82,51 @@
 										$scope.elfin.IDENTIFIANT.VALEUR = valneuf;
 									} 
 								};
+								
+								
+								// Search prestation for no SAI, groupe prestation, year
+								$scope.getPrestation = function(sai,groupePrestation,year) {
+									
+									//elfin.IDENTIFIANT.OBJECTIF
+									//elfin.CARACTERISTIQUE.CAR1.UNITE
+									//elfin.IDENTIFIANT.PAR
+									$log.debug("getPrestation = " + "sai = " + sai +", groupePrestation = " + groupePrestation + ", year = " + year);
+									// TODO: move prestationCollectionId to constants.
+									var prestationCollectionId = "G20081113902512302";
+									var xpathForPrestation = "//ELFIN[@GROUPE='"+groupePrestation+"' and IDENTIFIANT/DE='"+year+"' and starts-with(IDENTIFIANT/OBJECTIF, '"+sai+".')]";
+									
+									// Asychronous buildings preloading
+									GeoxmlService
+											.getCollection(prestationCollectionId)
+											.getList({
+												"xpath" : xpathForPrestation
+											})
+											.then(
+													function(prestations) {
+														
+														if (prestations.length == 1) {
+															$scope.constatPrestation = prestations[0];
+															$scope.elfin.IDENTIFIANT.COMPTE = $scope.constatPrestation.IDENTIFIANT.COMPTE;
+															$scope.elfin.IDENTIFIANT.OBJECTIF = $scope.constatPrestation.IDENTIFIANT.OBJECTIF;
+															$log.debug(">>> : $scope.constatPrestation = " + $scope.constatPrestation);
+														} else if (prestations.length < 1) {
+															hbAlertMessages.addAlert(
+																	"warning", "Pas de prestation correspondant aux informations: SAI = " + sai + ", groupe de prestation = " + groupePrestation + ", année = " + year);
+														} else if (prestations.length > 1) {
+															hbAlertMessages.addAlert(
+																	"warning", "Plusieurs prestations correspondent aux informations: SAI = " + sai + ", groupe de prestation = " + groupePrestation + ", année = " + year);															
+														}
+													},
+													function(response) {
+														var message = "Le chargement de la PRESTATION correspondant aux informations: SAI = " + sai + ", groupe de prestation = " + groupePrestation + ", année = " + year + " a échoué (statut de retour: "
+																+ response.status
+																+ ")";
+														hbAlertMessages.addAlert(
+																"danger", message);
+													});
+									
+								};								
+								
 
 								$rootScope
 										.$on(
