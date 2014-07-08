0544753d9ca433f9d9fad10664db08aff034411f
refond <refon@pobox.com>
2014-07-08 16:42:22 +0200

    Sort ECHEANCE.

--- main/src/hbConstatCardController.js
+++ main/src/hbConstatCardController.js
@@ -46,7 +46,22 @@
 								// Do nothing
 							}			            	
 			            	
-			            });						
+			            });
+
+			            /**
+			             * Perform operations on current CONSTAT elfin once available
+			             */
+				    	$scope.$watch('elfin.Id', function() { 
+
+				    		if ($scope.elfin!=null) {
+					            $log.debug("ABOUT TO SORT ECHEANCE ...");
+					            if ($scope.elfin.ACTIVITE.EVENEMENT.ECHEANCE && $scope.elfin.ACTIVITE.EVENEMENT.ECHEANCE.length > 1) {
+						            $log.debug("Sorting "+$scope.elfin.ACTIVITE.EVENEMENT.ECHEANCE.length+" ECHEANCEs");
+					            	hbUtil.reorderArrayByPOS($scope.elfin.ACTIVITE.EVENEMENT.ECHEANCE);
+					            };
+				    		};
+				    		
+				    	}, true);
 						
 			            $scope.deadlinePredicate = 'DATE';
 			            $scope.deadlineReverse = true;
