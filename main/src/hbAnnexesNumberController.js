009873691c6de5ae0702da3654c325b2cee95fa6
refond <refon@pobox.com>
2014-07-20 20:46:46 +0200

    Code clean up.

--- main/src/hbAnnexesNumberController.js
+++ main/src/hbAnnexesNumberController.js
@@ -12,20 +12,15 @@
 							'HB_EVENTS',
 							function($rootScope, $scope, $log, $filter, HB_EVENTS) {
 
-								$log.debug("    >>>> HbAnnexesNumberController called... ");
-
 					            $scope.refresh = function() {
-									if (!angular.isUndefined($scope.elfin) && !angular.isUndefined($scope.elfin.ANNEXE) && !angular.isUndefined($scope.elfin.ANNEXE.RENVOI)) {
+									if (!(angular.isUndefined($scope.elfin) || $scope.elfin===null) && !angular.isUndefined($scope.elfin.ANNEXE) && !angular.isUndefined($scope.elfin.ANNEXE.RENVOI)) {
 										$scope.annexesNoPhoto = $filter('annexExcludeTag')($scope.elfin.ANNEXE.RENVOI, 'photo');
-										$log.debug("    >>>> HbAnnexesNumberController - RENVOI DEFINED = " + $scope.annexesNoPhoto.length);
 									} else {
-										$log.debug("    >>>> HbAnnexesNumberController - RENVOI NOT DEFINED");
 										$scope.annexesNoPhoto = new Array();
 									}
 					            };
 					            
 					            $scope.$watch('elfin.ANNEXE.RENVOI', function() { 
-					            	$log.debug("    >>>> HbAnnexesNumberController - watching elfin.ANNEXE.RENVOI");
 					            	$scope.refresh();
 					            }, true);
 					            
