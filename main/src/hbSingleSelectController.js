0ce5a79a19a0d7faffb89ac10921a09a07f765fb
refond <refon@pobox.com>
2014-09-17 14:29:39 +0200

    Added comments.

--- main/src/hbSingleSelectController.js
+++ main/src/hbSingleSelectController.js
@@ -9,21 +9,31 @@
 					'hbUtil',
 					function($scope, $log, $filter, hbUtil) {
 
-						// Reference to hb-single-select ng-model sibling directive controller
+						/**
+						 * Reference to hb-single-select ng-model sibling directive controller
+						 */
 						$scope.ngModelCtrl = null;
 
+						/**
+						 * ngModel controller scope reference setter.
+						 */
 						this.setNgModelCtrl = function(ctrl) {
 							$scope.ngModelCtrl = ctrl;						
 						};
 						
+						/**
+						 * Reflects user selection to model view value and simulate user input with commit action.
+						 */
 						$scope.selectOption = function(option) {
 							// Reflect user selection to model.
 							$scope.ngModelCtrl.$viewValue = option.name;
 							$scope.ngModelCtrl.$commitViewValue();
 						};
 
-						
-						// Listen to ngModelCtrl model value change. (reference to method is safe thanks angular.noop)
+						/**
+						 * Listen to ngModelCtrl model value change and reflect it to the view.
+						 * Note: Reference to method is safe thanks to angular.noop
+						 */
 						$scope.$watch('ngModelCtrl.$modelValue', function (newValue, oldValue) {
 							$scope.ngModelCtrl.$viewValue = $scope.ngModelCtrl.$modelValue;
 						}, true);										
