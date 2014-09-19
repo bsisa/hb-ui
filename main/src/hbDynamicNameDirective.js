800157a79aafbce3660db65037dd692e3129fef6
refond <refon@pobox.com>
2014-09-19 18:34:57 +0200

    Directive to allow use of dynamically evaluated HTML input name attribute.

---
+++ main/src/hbDynamicNameDirective.js
@@ -0,0 +1,18 @@
+(function() {
+    /**
+     * Directive to allow use of dynamically evaluated HTML input name attribute.
+     */
+    angular.module('hb5').directive('hbDynamicName', function ($compile, $parse) {
+        return {
+            restrict: 'A',
+            priority: 9999999, // We would like it processed first in case another directive relies on HTML input name attribute.
+            link: function(scope, element) {
+                var nameValue = $parse(element.attr('hb-dynamic-name'))(scope);
+                element.removeAttr('hb-dynamic-name');
+                element.attr('name', nameValue);
+                $compile(element)(scope);
+            }
+        };
+    });
+
+})();
