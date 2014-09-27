e7de1b11259c4a039c2e7d8fe6e1686c43c3c313
refond <refon@pobox.com>
2014-09-28 01:12:24 +0200

    Making use of new hb-choose-actor-elfin-model hb-choose-actor directive scope property allows light weight hb-actor-line-converter controller without REST API query.

--- main/src/hbActorLineConverterController.js
+++ main/src/hbActorLineConverterController.js
@@ -16,37 +16,9 @@
 								Id : "",
 								ID_G : "", 
 								GROUPE : "", 
-								NOM : "",
-								IDENTIFIANT : { 
-									QUALITE : ""
-									}
+								NOM : ""
 								}; 
 						
-						/**
-						 * Load ELFIN given its collectionId (ELFIN.ID_G) and elfinId (ELFIN.Id)
-						 */
-				        $scope.getElfinActor = function (collectionId, elfinId) {
-				        	
-				        	// Do not perform call without valid ids.
-				        	if (GeoxmlService.validateId(collectionId) && GeoxmlService.validateId(elfinId)) {				        	
-						        GeoxmlService.getElfin(collectionId, elfinId).get()
-						        .then(function(actorElfin) {
-						        	// Force CAR array sorting by POS attribute
-						        	// TODO: Evaluate how to guarantee this in the produced JSON on the server in a single place.
-						        	// DONE: Safe array ordering is mandatory to prevent null accessor related exception
-						        	//       Need review of other similar operations
-						        	if ( actorElfin['CARACTERISTIQUE'] != null && actorElfin['CARACTERISTIQUE']['CARSET'] != null && actorElfin['CARACTERISTIQUE']['CARSET']['CAR'] != null) {
-						        		hbUtil.reorderArrayByPOS(actorElfin['CARACTERISTIQUE']['CARSET']['CAR']);
-						        	}
-						        	$scope.actorModel.IDENTIFIANT.QUALITE = actorElfin.IDENTIFIANT.QUALITE; 
-						        }, function(response) {
-						        	var message = "Aucun object ACTEUR disponible pour la collection: " + collectionId + " et l'identifiant: " + elfinId + ".";
-						        	$log.warn("HbChooseActorController - statut de retour: " + response.status + ". Message utilisateur: " + message);
-						        });
-				        	};
-			        		
-			        	};							
-
 			        	/**
 			        	 * Listen to FRACTION.L modification (initialisation). actor.Id is stored at C[2].VALUE
 			        	 * and set actorModel. 
@@ -56,8 +28,6 @@
 			            	$scope.actorModel.Id = $scope.lineModel.C[2].VALUE;
 			            	$scope.actorModel.GROUPE = $scope.lineModel.C.length > 3 ? $scope.lineModel.C[3].VALUE : "";
 			            	$scope.actorModel.NAME = $scope.lineModel.C.length > 4 ? $scope.lineModel.C[4].VALUE : "";
-			            	// Add ACTEUR properties not part of PERSONNEType such as IDENTIFIANT.QUALITE (actor role).
-			            	$scope.getElfinActor($scope.actorModel.ID_G,$scope.actorModel.Id);
 						}, true);						
 						
 						/**
@@ -74,8 +44,6 @@
 				            	$scope.lineModel.C[3].VALUE = $scope.actorModel.GROUPE;
 				            	$scope.lineModel.C[4].VALUE = $scope.actorModel.NAME;			            		
 			            	}
-			            	// Add ACTEUR properties not part of PERSONNEType such as IDENTIFIANT.QUALITE (actor role).
-			            	$scope.getElfinActor($scope.actorModel.ID_G,$scope.actorModel.Id);
 						}, true);
 	
 					} ]);
