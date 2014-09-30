16046bfde2587e110511b983f1b7697fe3997708
refond <refon@pobox.com>
2014-09-30 16:25:31 +0200

    Fixed wrong length tests.

--- main/src/hbActorLineConverterController.js
+++ main/src/hbActorLineConverterController.js
@@ -24,16 +24,18 @@
 			        	 * and set actorModel. 
 			        	 */
 						$scope.$watch('lineModel.C[2]', function (newValue, oldValue) {
+							// C[0] corresponding to POS="1" is unused.
 			            	$scope.actorModel.ID_G = $scope.lineModel.C[1].VALUE;
 			            	$scope.actorModel.Id = $scope.lineModel.C[2].VALUE;
-			            	$scope.actorModel.GROUPE = $scope.lineModel.C.length > 3 ? $scope.lineModel.C[3].VALUE : "";
-			            	$scope.actorModel.NAME = $scope.lineModel.C.length > 4 ? $scope.lineModel.C[4].VALUE : "";
+			            	$scope.actorModel.GROUPE = $scope.lineModel.C.length > 2 ? $scope.lineModel.C[3].VALUE : "";
+			            	$scope.actorModel.NAME = $scope.lineModel.C.length > 3 ? $scope.lineModel.C[4].VALUE : "";
 						}, true);						
 						
 						/**
 						 * Listen to actorModel update to be reflected to lineModel
 						 */
 						$scope.$watch('actorModel', function (newValue, oldValue) {
+							// C[0] corresponding to POS="1" is unused.
 							$scope.lineModel.C[1].VALUE = $scope.actorModel.ID_G;
 			            	$scope.lineModel.C[2].VALUE = $scope.actorModel.Id;
 			            	// Deal with incomplete data.
