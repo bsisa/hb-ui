d4dcc7b48ce92ce72050ce6d35b860024460b540
refond <refon@pobox.com>
2014-10-02 14:10:29 +0200

    Manage Prestation I, Prestation II and refactor layout.

--- main/src/hbContratCardController.js
+++ main/src/hbContratCardController.js
@@ -6,6 +6,7 @@
 					'HbContratCardController',
 					[
 							'$scope',
+							'$attrs',
 							'GeoxmlService',
 							'$modal',
 							'$routeParams',
@@ -15,13 +16,47 @@
 							'hbAlertMessages',
 							'hbUtil',
 							'hbQueryService',
-							function($scope, GeoxmlService, $modal,
+							function($scope, $attrs, GeoxmlService, $modal,
 									$routeParams, $location, $log, $timeout,
 									hbAlertMessages, hbUtil, hbQueryService) {
 
 								$log
 										.debug("    >>>> Using HbContratCardController");
 
+								
+						    	// Check when elfin instance becomes available 
+						    	$scope.$watch('elfin.Id', function() { 
+						    		
+						    		if ($scope.elfin!=null) {
+						    			/**
+						    			 * Perform template clean up tasks while in create mode.
+						    			 */
+							    		if ($attrs.hbMode === "create") {
+							    			if ($routeParams.sai) {
+							    				$scope.elfin.IDENTIFIANT.OBJECTIF = $routeParams.sai;
+							    				// Reset choices list init. values.
+							    				$scope.elfin.CARACTERISTIQUE.CAR1.UNITE = '';
+							    				$scope.elfin.CARACTERISTIQUE.CAR1.VALEUR = '';
+							    			}
+							    			$scope.elfin.GROUPE = "";
+							    		}
+							    		
+						    		};
+						    		
+						    	}, true);								
+								
+						    	GeoxmlService.getNewElfin("CONTRAT").get()
+					            .then(function(contrat) {
+					            		// Get groupeChoices from catalogue default
+					            	    $scope.groupeChoices = hbUtil.buildArrayFromCatalogueDefault(contrat.GROUPE);
+					            	    $scope.prestation_IChoices = hbUtil.buildArrayFromCatalogueDefault(contrat.CARACTERISTIQUE.CAR1.UNITE);
+									},
+									function(response) {
+										var message = "Les valeurs par défaut pour la CLASSE CONTRAT n'ont pas pu être chargées. (statut de retour: "+ response.status+ ")";
+										hbAlertMessages.addAlert("danger",message);
+									});								
+								
+								
 								var xpathForEntreprises = "//ELFIN[IDENTIFIANT/QUALITE='Entreprise']";
 								$scope.entrepriseActors = null;
 								hbQueryService.getActors(xpathForEntreprises)
@@ -36,6 +71,13 @@
 													hbAlertMessages.addAlert(
 															"danger", message);
 												});
+								
+
+					            // Parameters to hbChooseOne service function for ACTOR selection
+					            $scope.actorChooseOneColumnsDefinition = [
+						                        		   		            { field:"GROUPE", displayName: "Groupe"}
+						                        		   	 		   		];
+					            $scope.actorChooseOneTemplate = '/assets/views/chooseOneActor.html';								
 
 							} ]);
 
