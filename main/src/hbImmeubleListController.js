30f2d48eeca3c1039810979a845520492b338028
refond <refon@pobox.com>
2014-11-17 22:45:02 +0100

    Moved from page defined filtering to controller defined filtering to get access to $scope.filteredElfins.length to provide current selection size to end user.

--- main/src/hbImmeubleListController.js
+++ main/src/hbImmeubleListController.js
@@ -1,6 +1,6 @@
 (function() {
 
-    angular.module('hb5').controller('HbImmeubleListController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, hbAlertMessages, hbUtil) {
+    angular.module('hb5').controller('HbImmeubleListController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', '$filter', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, $filter, hbAlertMessages, hbUtil) {
     
     	$log.debug("    >>>> HbImmeubleListController called...");
     	
@@ -17,6 +17,48 @@
     			"address" : "",
     			"text" : ""
     	};
+    	
+    	
+    	
+		/**
+		 * Proceed to elfin_p collection `prestationListFilter` filtering and sorting
+		 * Moved from page defined filtering: 
+		 * immeubleListFilter:search | immeubleListAnyFilter:search.text | orderBy:predicate:reverse
+		 * to controller defined filtering to have access to $scope.filteredElfins.length
+		 */
+		var filterSortElfins = function(elfins_p, search_p, predicate_p, reverse_p) {
+			// Apply prestationListFilter
+	    	var filteredSortedElfins = $filter('immeubleListFilter')(elfins_p, search_p);
+	    	filteredSortedElfins = $filter('immeubleListAnyFilter')(filteredSortedElfins, search_p.text);
+	    	// Apply predicate, reverse sorting
+	    	filteredSortedElfins = $filter('orderBy')(filteredSortedElfins, predicate_p, reverse_p);
+	    	return filteredSortedElfins;
+		};
+    	
+		
+		/**
+		 * Update filtered collection when search or sorting criteria are modified. 
+		 */
+    	$scope.$watch('[search,predicate,reverse]', function(newSearch, oldSearch) {
+    		//$log.debug(">>>>> HbImmeubleListController search, predicate or reverse UPDATED <<<<< \n" + angular.toJson(newSearch) );
+    		if ($scope.elfins!=null) {
+				$scope.filteredElfins = filterSortElfins($scope.elfins, $scope.search, $scope.predicate, $scope.reverse);
+    		}
+    	}, true);								
+		
+		/**
+		 * elfins can result from queries taking possibly seconds to tens of seconds to complete.
+		 * This requires watching for elfins result availability before computing filteredElfins.length. 
+		 */
+    	$scope.$watch('elfins', function() { 
+    		if ($scope.elfins!=null) {
+				$scope.filteredElfins = filterSortElfins($scope.elfins, $scope.search, $scope.predicate, $scope.reverse);										
+    		} else {
+    			//$log.debug(">>>>> HbImmeubleListController elfins NOT YET LOADED <<<<<");
+    		}
+    	});		
+		
+		
     	
     }]);
 
