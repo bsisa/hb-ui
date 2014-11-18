c53e7c97735b798012e43dc6339e719126754c10
refond <refon@pobox.com>
2014-11-18 17:12:31 +0100

    Dashboard - IMMEUBLE search and navigation implemented.

--- main/src/hbDashboardController.js
+++ main/src/hbDashboardController.js
@@ -1,158 +1,87 @@
 (function() {
 
-    angular.module('hb5').controller('HbDashboardController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', '$filter', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, $filter, hbAlertMessages, hbUtil) {
+    angular.module('hb5').controller('HbDashboardController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', '$filter', '$location', '$timeout', 'HB_COLLECTIONS', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, $filter, $location, $timeout, HB_COLLECTIONS, hbAlertMessages, hbUtil) {
     
-    	$log.debug("    >>>> HbDashboardController called...");
-    	
-    	
-    	var immeublesCollectionId = 'G20040930101030005';
-    	
-        // Contains an ELFINs JSON Array resulting from the GeoxmlService query   
-        $scope.elfins = null;
-        $scope.elfinsCount = null;
-        
-        if (GeoxmlService.validateId(immeublesCollectionId)) {
-        	
-        	if ($routeParams.xpath) {
-                GeoxmlService.getCollection(immeublesCollectionId).getList({"xpath" : $routeParams.xpath})
-                .then(function(elfins) {
-                	if (elfins == null) {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = 0;
-                	} else {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = elfins.length;
-                	}
-                }, function(response) {
-                    var message = "Le chargement des informations a échoué (statut de retour: " + response.status + ")";
-                    hbAlertMessages.addAlert("danger",message);
-                });        		
-        	} else if ($attrs.hbElfinClasse) {
-
-        		var elfinClasseXpathRestriction = hbUtil.encodeUriParameter("//ELFIN[@CLASSE='"+$attrs.hbElfinClasse+"']");
-        		
-                GeoxmlService.getCollection(immeublesCollectionId).getList({"xpath" : elfinClasseXpathRestriction})
-                .then(function(elfins) {
-                	if (elfins == null) {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = 0;
-                	} else {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = elfins.length;
-                	}
-                }, function(response) {
-                    var message = "Le chargement des informations a échoué (statut de retour: " + response.status + ")";
-                    hbAlertMessages.addAlert("danger",message);
-                });	
-        	} else {
-                GeoxmlService.getCollection(immeublesCollectionId).getList()
-                .then(function(elfins) {
-                	if (elfins == null) {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = 0;
-                	} else {
-                		$scope.elfins = elfins;
-                		$scope.elfinsCount = elfins.length;
-                	}
-                }, function(response) {
-                    var message = "Le chargement des informations a échoué (statut de retour: " + response.status + ")";
-                    hbAlertMessages.addAlert("danger",message);
-                });	
-        	}
-        } else {
-            var message = "L'identifiants de collection (" + immeublesCollectionId + " ) n'est pas correct.";
-            hbAlertMessages.addAlert("warning",message);
-        }    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
+    	$log.debug("    >>>> HbDashboardController called... at " + new Date());
     	
     	
     	// ============================================================
-        // Buildings search 
-    	// ============================================================
+    	// Buildings Section - IMMEUBLE
+    	// ============================================================    	
     	
-    	// Default order is by "Building management" 
-    	$scope.predicate = 'IDENTIFIANT.OBJECTIF';
-    	$scope.reverse = false;
-
-    	// Object holding user entered search (filter) criteria 
-    	$scope.search = {
-    			"owner" : "",
-    			"registerNb" : "",
-    			"place" : "",
-    			"buildingNb" : "",
-    			"address" : "",
-    			"text" : ""
-    	};
-    	
-    	
+    	// ==== Initialisation ========================================
+    	var immeublesCollectionId = HB_COLLECTIONS.IMMEUBLE_ID;
     	
 		/**
-		 * Proceed to elfin_p collection `prestationListFilter` filtering and sorting
-		 * Moved from page defined filtering: 
-		 * immeubleListFilter:search | immeubleListAnyFilter:search.text | orderBy:predicate:reverse
-		 * to controller defined filtering to have access to $scope.filteredElfins.length
+		 *  Apply immeubleListAnyFilter
 		 */
-		var filterSortElfins = function(elfins_p, search_p, predicate_p, reverse_p) {
-			// Apply prestationListFilter
-	    	var filteredSortedElfins = $filter('immeubleListFilter')(elfins_p, search_p);
-	    	filteredSortedElfins = $filter('immeubleListAnyFilter')(filteredSortedElfins, search_p.text);
-	    	// Apply predicate, reverse sorting
-	    	filteredSortedElfins = $filter('orderBy')(filteredSortedElfins, predicate_p, reverse_p);
+		var filterImmeubleElfins = function(elfins_p, search_p) {
+	    	var filteredSortedElfins = $filter('immeubleListAnyFilter')(elfins_p, search_p.text);
 	    	return filteredSortedElfins;
-		};
+		};    	
     	
+        // Contains ELFINs JSON Array resulting from the GeoxmlService query   
+        $scope.immeubleElfins = null;
+        
+        // Query all available buildings IMMEUBLE 
+        GeoxmlService.getCollection(immeublesCollectionId).getList()
+	        .then(function(immeubleElfins) {
+        		$scope.immeubleElfins = immeubleElfins;
+	        }, function(response) {
+	            var message = "Le chargement des immeubles a échoué (statut de retour: " + response.status + ")";
+	            hbAlertMessages.addAlert("danger",message);
+	        });	
+    	
+		/**
+		 * immeubleElfins result is loaded asynchronously.
+		 */
+    	$scope.$watch('immeubleElfins', function() { 
+    		if ($scope.immeubleElfins!=null) {
+				$scope.filteredImmeubleElfins = filterImmeubleElfins($scope.immeubleElfins, $scope.immeubleSearch);										
+    		}
+    	});	        
+
+    	// ==== Navigation ===========================================
+        /**
+         * Navigate to user selected list
+         */
+        $scope.listImmeubles = function() {
+        	$location.path('/elfin/'+immeublesCollectionId+'/IMMEUBLE').search('search', $scope.immeubleSearch.text);
+        };
+        /**
+         * Navigate to user selected building (IMMEUBLE)
+         */        
+        $scope.viewImmeuble = function() {
+        	$location.path('/elfin/'+immeublesCollectionId+'/IMMEUBLE/' + $scope.filteredImmeubleElfins[0].Id);
+        };        
+
+    	// ==== End user search and related listener ==================        
+        /** User entered IMMEUBLE search criterion */
+        $scope.immeubleSearch = { "text" : "" };
 		
 		/**
 		 * Update filtered collection when search or sorting criteria are modified. 
 		 */
-    	$scope.$watch('[search,predicate,reverse]', function(newSearch, oldSearch) {
-    		//$log.debug(">>>>> HbImmeubleListController search, predicate or reverse UPDATED <<<<< \n" + angular.toJson(newSearch) );
-    		if ($scope.elfins!=null) {
-				$scope.filteredElfins = filterSortElfins($scope.elfins, $scope.search, $scope.predicate, $scope.reverse);
+    	$scope.$watch('immeubleSearch', function(newSearch, oldSearch) {
+    		if ($scope.immeubleElfins!=null) {
+				$scope.filteredImmeubleElfins = filterImmeubleElfins($scope.immeubleElfins, $scope.immeubleSearch);
     		}
     	}, true);								
-		
-		/**
-		 * elfins can result from queries taking possibly seconds to tens of seconds to complete.
-		 * This requires watching for elfins result availability before computing filteredElfins.length. 
-		 */
-    	$scope.$watch('elfins', function() { 
-    		if ($scope.elfins!=null) {
-				$scope.filteredElfins = filterSortElfins($scope.elfins, $scope.search, $scope.predicate, $scope.reverse);										
-    		} else {
-    			//$log.debug(">>>>> HbImmeubleListController elfins NOT YET LOADED <<<<<");
-    		}
-    	});		
-		
-    	
+			
     	// ============================================================
         // Buildings search - end
     	// ============================================================    	
 		
+    	
+    	
+    	
+    	var focusOnSearchField = function() {
+			$('#immeubleSearchTextInput').focus();	
+		};        
+		
+		// TODO: FocusTimeout issue. Find a better solution ? 
+		$timeout(focusOnSearchField, 250, false);    	
+    	
     	
     }]);
 
