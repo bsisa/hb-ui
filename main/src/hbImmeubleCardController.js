f4af541d03bca2223de7843acdd350b46e7783d9
refond <refon@pobox.com>
2015-01-24 16:24:57 +0100

    PRODUCTION_CHALEUR need to match source IMMEUBLE OBJECTIF and ORIGINE. See IMMEUBLE no sai 192 for a use case.

--- main/src/hbImmeubleCardController.js
+++ main/src/hbImmeubleCardController.js
@@ -263,8 +263,7 @@
 							    		if ($scope.elfin!=null && $attrs.hbMode != "create") {
 								            // TODO: productionChaleurCollectionId must come from server configuration resource.
 							    			// DONE: added restriction on PROPRIETAIRE, CLASSE. Strict restriction on OBJECTIF, starts-with is not correct in all cases.
-//								            var xpathForProductionChaleur = "//ELFIN[IDENTIFIANT/OBJECTIF='"+$scope.elfin.IDENTIFIANT.OBJECTIF+"' and IDENTIFIANT/NOM='"+$scope.elfin.IDENTIFIANT.NOM+"' and @CLASSE='PRODUCTION_CHALEUR']";
-								            var xpathForProductionChaleur = "//ELFIN[IDENTIFIANT/OBJECTIF='"+$scope.elfin.IDENTIFIANT.OBJECTIF+"' and @CLASSE='PRODUCTION_CHALEUR']";								            
+								            var xpathForProductionChaleur = "//ELFIN[IDENTIFIANT/OBJECTIF='"+$scope.elfin.IDENTIFIANT.OBJECTIF+"' and IDENTIFIANT/ORIGINE='"+$scope.elfin.IDENTIFIANT.NOM+"' and @CLASSE='PRODUCTION_CHALEUR']";
 								            hbQueryService.getProductionChaleurList(xpathForProductionChaleur)
 												.then(function(elfins) {
 														$scope.productionChaleurList = elfins;
