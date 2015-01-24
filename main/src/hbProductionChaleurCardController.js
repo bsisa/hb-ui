0d212321e1b52f900fa1511deee565604d9b85f3
refond <refon@pobox.com>
2015-01-24 17:48:46 +0100

    Provide navigation back to IMMEUBLE.

--- main/src/hbProductionChaleurCardController.js
+++ main/src/hbProductionChaleurCardController.js
@@ -12,11 +12,52 @@
 					'$log',
 					'hbAlertMessages',
 					'hbUtil',
+					'hbQueryService',
 					function($attrs, $scope, GeoxmlService, $modal, $routeParams,
-							$location, $log, hbAlertMessages, hbUtil) {
+							$location, $log, hbAlertMessages, hbUtil, hbQueryService) {
 
 						//$log.debug("    >>>> Using HbProductionChaleurCardController ");
 						
+						// Used to provide navigation link back to IMMEUBLE link to this CONSTAT
+						$scope.immeubleRef = null;			 			
+			 			
+			        	// Navigate to IMMEUBLE linked to the current CONSTAT
+			        	$scope.viewImmeuble = function() {
+			        		$location.path('/elfin/'+$scope.immeubleRef.ID_G+'/IMMEUBLE/' + $scope.immeubleRef.Id);	
+			        	};
+			 			
+			        	
+				    	$scope.$watchCollection('[elfin.IDENTIFIANT.OBJECTIF,elfin.IDENTIFIANT.ORIGINE]', function(newValues, oldValues) {							    		
+
+				    		$log.debug("$watchCollection for OBJECTIF, ORIGINE : " + oldValues[0] + ", " + oldValues[1] + " => " + newValues[0] + ", " + newValues[1]);
+				    		
+				    		if ($scope.elfin!=null && $attrs.hbMode != "create") {
+					    		var saiNb = $scope.elfin.IDENTIFIANT.OBJECTIF;
+								var buildingNb = $scope.elfin.IDENTIFIANT.ORIGINE;
+								xpathForImmeubleLinkedToCurrentConstat = "//ELFIN[IDENTIFIANT/OBJECTIF='"+saiNb+"' and IDENTIFIANT/NOM='"+buildingNb+"']";
+								hbQueryService.getImmeubles(xpathForImmeubleLinkedToCurrentConstat)
+									.then(function(immeubles) {
+										// Expected case
+										if (immeubles.length === 1) {
+											var immeuble = immeubles[0];
+											$scope.immeubleRef = { "Id" : immeuble.Id,  "ID_G" : immeuble.ID_G , "address": immeuble.IDENTIFIANT.ALIAS, "owner": immeuble.PARTENAIRE.PROPRIETAIRE.NOM};
+										} else if (immeubles.length < 1) { 
+											var message = "L'IMMEUBLE correspondant à la PRODUCTION_CHALEUR courrante pour le no de construction "+ buildingNb +" et no SAI "+ saiNb +" n'a pas pu être trouvé dans la base de donnée!";
+								            hbAlertMessages.addAlert("danger",message);
+										} else {
+											var message = "Plusieurs IMMEUBLE correspondent à la PRODUCTION_CHALEUR courrante pour le no de construction: " + buildingNb +" et no SAI "+ saiNb +". Le résultat devrait être unique.";
+								            hbAlertMessages.addAlert("danger",message);
+										}
+									},
+									function(response) {
+										var message = "Le chargement de l'IMMEUBLE lié à la PRODUCTION_CHALEUR courrante pour le no de construction "+ buildingNb +" et no SAI "+ saiNb + " a échoué (statut de retour: "+ response.status+ ")";
+							            hbAlertMessages.addAlert("danger",message);
+									});										
+				    			
+				    		}
+				    	}, true);
+			        	
+			 			
 						// Get available roles dynamically from HB5 catalogue
 				    	$scope.$watch('elfin.Id', function() { 
 
@@ -38,13 +79,12 @@
 								    //$scope.elfin.IDENTIFIANT.QUALITE = "";
 								} else {
 									// Do nothing
+									
 								}					            
-				    		};
-				    		
-				    	}, true);						
-						
-						
-						
+								
+								
+				    		};	
+				    	}, true);												
 					} ]);
 	
 
