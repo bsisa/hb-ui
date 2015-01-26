c2b74b21f3fb09b4115b1b56d307c5b0f16a8bc9
refond <refon@pobox.com>
2015-01-26 16:44:19 +0100

    Formatting.

--- main/src/hbFormeComponentController.js
+++ main/src/hbFormeComponentController.js
@@ -1,23 +1,23 @@
 (function() {
 
-	    angular
-			.module('hb5')
-			.controller(
-					'HbFormeComponentController',
-					[
-							'$scope',
-							'$log',
-							function($scope, $log) {
-    
-									//$log.debug("    >>>> Using HbFormeComponentController");
-							        
-									// Manage geographic or schematic mode.
-									$scope.COORDINATE_TYPE = {
-											GEOGRAPHIC : {label: "Géographique", css: "panel-primary"},
-											SCHEMATIC :  {label: "Schématique", css: "panel-success"}
-									};
-									$scope.coordinateType = $scope.COORDINATE_TYPE.GEOGRAPHIC;
-									        
-							    } ]);
+	angular.module('hb5').controller('HbFormeComponentController',
+			[ '$scope', '$log', function($scope, $log) {
+
+				// $log.debug(" >>>> Using HbFormeComponentController");
+
+				// Manage geographic or schematic mode.
+				$scope.COORDINATE_TYPE = {
+					GEOGRAPHIC : {
+						label : "Géographique",
+						css : "panel-primary"
+					},
+					SCHEMATIC : {
+						label : "Schématique",
+						css : "panel-success"
+					}
+				};
+				$scope.coordinateType = $scope.COORDINATE_TYPE.GEOGRAPHIC;
+
+			} ]);
 
 })();
