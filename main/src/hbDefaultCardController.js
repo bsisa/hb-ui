35762e6193814efbe954cf77924a92d1c88098af
refond <refon@pobox.com>
2015-02-20 02:46:08 +0100

    Fix uploaded photo not appearing on default card.

--- main/src/hbDefaultCardController.js
+++ main/src/hbDefaultCardController.js
@@ -19,6 +19,20 @@
 						// we do not expect to have much logic here which shall not 
 						// go to hbCardContainerController. 
 						
+						
+				    	// Check when elfin instance becomes available 
+				    	$scope.$watch('elfin.Id', function() { 
+				    		
+				    		if ($scope.elfin!=null) {						
+						
+					            // Make IMMEUBLE photo available
+					            $scope.updatePhotoSrc();
+			            
+				    		};
+				    		
+				    	}, true);
+						
+						
 					} ]);
 	
 
