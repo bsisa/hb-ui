8e3849e6162ecdf6df28118b631b686547c05165
refond <refon@pobox.com>
2015-05-27 10:40:36 +0200

    Revert to hb-dynamic-name usage after refactoring directive declaration syntax not to break while javascript is minified. Linked to infamous 'Unknown provider: aProvider <- a' error.

--- main/src/hbDynamicNameDirective.js
+++ main/src/hbDynamicNameDirective.js
@@ -2,7 +2,7 @@
     /**
      * Directive to allow use of dynamically evaluated HTML input name attribute.
      */
-    angular.module('hb5').directive('hbDynamicName', function ($compile, $parse) {
+    angular.module('hb5').directive('hbDynamicName', ['$compile', '$parse', function ($compile, $parse) {
         return {
             restrict: 'A',
             priority: 9999999, // We would like it processed first in case another directive relies on HTML input name attribute.
@@ -13,6 +13,7 @@
                 $compile(element)(scope);
             }
         };
-    });
+    }]);
+    	                 
 
 })();
