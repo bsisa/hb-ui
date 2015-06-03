32eae2c0653227c3f1bf12a821fcd71d20889542
refond <refon@pobox.com>
2015-06-03 15:46:34 +0200

    Add documentation.

--- main/src/hbChooseActorDirective.js
+++ main/src/hbChooseActorDirective.js
@@ -3,7 +3,49 @@
 
     /**
      * hb-choose-actor directive provides specific popup to select 
-     * an ELFIN of CLASSE='ACTEUR' with IDENTIFIANT/QUALITE='{"collaborateur, propriétaire,..."}'
+     * an ELFIN of CLASSE='ACTEUR' 
+     * 
+     * 
+     * elfinForm
+     * 
+     * Is a reference to the current form the selection will be used to update to.
+     * 
+     * 
+     * actorModel 
+     * 
+     * Is a mandatory reference to a non persistent scope level javascript object with structure: {Id : "", ID_G : "", GROUPE : "", NOM : ""};
+     * It is simply used to old these properties value. They may be used to load the corresponding ELFIN
+     * of class ACTEUR or be copied as is to data structure such as FRACTION/L/C... 
+     * 
+     * 
+     * actorElfinModel 
+     * 
+     * Is an optional reference to the ELFIN of class ACTEUR
+     * corresponding to the actorModel ID_G/Id identifier. 
+     * 
+     * 
+     * actorIdName 
+     * 
+     * Dynamically defines the actor HTML input 'name' property.
+     * 
+     * 
+     * actorRole
+     * 
+     * Optional property used to narrow the available ACTEUR list to for instance with
+     * commas separated values corresponding to ACTEUR IDENTIFIANT/QUALITE values. 
+     * For instance: 
+     * To restrict ACTEUR to type 'owner': hb-choose-actor-role="owner"
+     * To restrict ACTEUR to type 'owner' and 'company': hb-choose-actor-role="owner, company"
+     * To restrict ACTEUR to type other than 'owner' and 'company': hb-choose-actor-role="!owner, !company"
+     * 
+     * 
+     * defaultByName 
+     * 
+     * Optional property which allows defining an ACTEUR entity by name as a default.
+     * For instance: hb-choose-actor-default-by-name="NE" provided an ACTEUR for which
+     * INDENTIFIANT/NAME matches 'NE'. 
+     * 
+     * tooltop, editable: straight forward.
      */
     angular.module('hb5').directive('hbChooseActor', function () {
 
