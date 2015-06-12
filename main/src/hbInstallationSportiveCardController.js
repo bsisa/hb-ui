1f4a66b5773ac4fc5b66e2266565e36d8fe132f5
refond <refon@pobox.com>
2015-06-12 12:22:54 +0200

    Get $scope.equipementsSportifs list.

--- main/src/hbInstallationSportiveCardController.js
+++ main/src/hbInstallationSportiveCardController.js
@@ -11,10 +11,11 @@
 					'$location',
 					'$log',
 					'hbAlertMessages',
+					'hbQueryService',
 					'hbUtil',
 					'userDetails',					
 					function($attrs, $scope, GeoxmlService, $modal, $routeParams,
-							$location, $log, hbAlertMessages, hbUtil, userDetails) {
+							$location, $log, hbAlertMessages, hbQueryService, hbUtil, userDetails) {
 
 						//$log.debug("    >>>> Using HbInstallationSportiveCardController ");
 						
@@ -46,16 +47,37 @@
 									$scope.elfin.IDENTIFIANT.AUT = userDetails.getAbbreviation();
 									$scope.elfin.IDENTIFIANT.DE = hbUtil.getDateInHbTextFormat(new Date());
 								} else {
-
-								}					            
-					            
-					            
-					            
+						    			// Try loading linked EQUIPEMENT_SPORTIF only if not in create mode.
+						    			var xpathForEquipementsSportifs = "//ELFIN[@CLASSE='EQUIPEMENT_SPORTIF' and @SOURCE='"+$scope.elfin.ID_G+"/"+$scope.elfin.CLASSE + "/" +$scope.elfin.Id +"']";
+							            hbQueryService.getEquipementsSportifs(xpathForEquipementsSportifs)
+											.then(function(elfins) {
+								    			$scope.equipementsSportifs = elfins;
+												},
+												function(response) {
+													var message = "Le chargement des EQUIPEMENT_SPORTIFs a échoué (statut de retour: "+ response.status+ ")";
+										            hbAlertMessages.addAlert("danger",message);
+												});
+					    		}					            
 				    		};
 				    		
 				    	}, true);
 						
-						
+				    	
+						/**
+						 * Triggers a redirection to the EQUIPEMENT_SPORTIF creation URL with current
+						 * INSTALLATION_SPORTIVE ID_G, CLASSE, Id information passed as parameters.
+						 * Must not be effective while in create mode (no association is 
+						 * relevant while the EQUIPEMENT_SPORTIF creation is ongoing/pending.)
+						 */ 
+						$scope.createNewEquipementSportif = function() {										
+							if ($attrs.hbMode != "create") {
+						        // id,classe,idg provide generic link to creation source/parent
+								var searchObj = {id: $scope.elfin.Id, classe: $scope.elfin.CLASSE, idg: $scope.elfin.ID_G }
+								$location.search(searchObj).path( "/elfin/create/EQUIPEMENT_SPORTIF" );
+							}
+						};					    	
+				    	
+				    	
 					} ]);
 	
 
