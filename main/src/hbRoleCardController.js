6cf5c5d1ad5418475ac1bbda9f1a2f6707d6620f
refond <refon@pobox.com>
2015-07-14 15:20:26 +0200

    Add new GROUPE, REMARQUE properties to ELFIN @CLASSE='ROLE' to provide fine grained role management to administrators.

--- main/src/hbRoleCardController.js
+++ main/src/hbRoleCardController.js
@@ -4,17 +4,54 @@
 			'HbRoleCardController',
 			[
 					'$scope',
+					'$attrs',
 					'GeoxmlService',
 					'$modal',
-					'$routeParams',
-					'$location',
 					'$log',
 					'hbAlertMessages',
 					'hbUtil',
-					function($scope, GeoxmlService, $modal, $routeParams,
-							$location, $log, hbAlertMessages, hbUtil) {
+					'hbQueryService',
+					function($scope, $attrs, GeoxmlService, $modal, 
+							$log, hbAlertMessages, hbUtil, hbQueryService) {
 
 						//$log.debug("    >>>> Using HbRoleCardController ");
+
+			            // Asychronous ROLE template preloading
+			            GeoxmlService.getNewElfin("ROLE").get()
+			            .then(function(role) {
+			            		// Get list from catalogue default
+			            		$scope.groupeChoices = hbUtil.buildArrayFromCatalogueDefault(role.GROUPE);
+							},
+							function(response) {
+								var message = "Les valeurs par défaut pour la CLASSE ROLE n'ont pas pu être chargées. (statut de retour: "+ response.status+ ")";
+								hbAlertMessages.addAlert("danger",message);
+							});								
+
+			            // Asychronous existing ROLEs preloading
+			            var xpathForRoles = "//ELFIN[@CLASSE='ROLE']";
+			            hbQueryService.getRoleList(xpathForRoles)
+						.then(function(roles) {
+								$scope.roles = roles;
+							},
+							function(response) {
+								var message = "Le chargement des ROLEs a échoué (statut de retour: "+ response.status+ ")";
+					            hbAlertMessages.addAlert("danger",message);
+							});			            
+						
+						
+						// Proceed with operations which require elfin to be loaded 
+				    	$scope.$watch('elfin.Id', function() { 
+
+				    		if ($scope.elfin!=null) {
+								if ( $attrs.hbMode === "create" ) {
+									// Reset default from catalogue
+									$scope.elfin.GROUPE = "";									
+								} else {
+									// Do nothing
+								}					            
+				    		};	
+				    	}, true);						
+						
 						
 						
 					} ]);
