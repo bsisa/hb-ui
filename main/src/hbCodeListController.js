bb8435160b29c28eda9aad857bdb7cdfa2601d16
refond <refon@pobox.com>
2015-09-22 18:21:16 +0200

    Set correct default sort field.

--- main/src/hbCodeListController.js
+++ main/src/hbCodeListController.js
@@ -2,10 +2,8 @@
 
     angular.module('hb5').controller('HbCodeListController', ['$attrs', '$scope', 'GeoxmlService', '$routeParams', '$log', 'hbAlertMessages', 'hbUtil', function($attrs, $scope, GeoxmlService, $routeParams, $log, hbAlertMessages, hbUtil) {
     
-    	$log.debug("    >>>> HbCodeListController called... " );
-
-    	// Default order is by "Group" 
-    	$scope.predicate = 'GROUPE';
+    	// Default order is by "code" which is store in NOM field. 
+    	$scope.predicate = 'IDENTIFIANT.NOM';
     	$scope.reverse = false;
 
     	// Object holding user entered search (filter) criteria 
@@ -13,8 +11,8 @@
     			"text" : ""
     	};    	
     	
-        $scope.$watch('elfins', function(newElfins, oldElfins) {
-        	if (newElfins) {
+//        $scope.$watch('elfins', function(newElfins, oldElfins) {
+//        	if (newElfins) {
         		// Note: underscore syntax is provided by underscore.js library.
 //        		var uniqueGroups = _.chain($scope.elfins).pluck('IDENTIFIANT').pluck('QUALITE').unique().value(); 
 //        		$scope.isCollaboratorOnly = (uniqueGroups.length == 1 && uniqueGroups[0] == 'Collaborateur');
@@ -23,8 +21,8 @@
 //        		$scope.isRespChauffageOnly = (uniqueGroups.length == 1 && uniqueGroups[0] == 'Responsable chauffage');
 //        		$scope.isManagerOnly = (uniqueGroups.length == 1 && uniqueGroups[0] == 'Gérant');
 //        		$scope.isMany = (uniqueGroups.length > 1 || uniqueGroups.length < 1);
-        	}
-        });
+//        	}
+//        });
         
     }]);
 
