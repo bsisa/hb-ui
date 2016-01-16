ee7114d64b1f2a124cb860d6db439d55d52d43df
refond <refon@pobox.com>
2016-01-16 01:27:09 +0100

    Improved UX with as you type order amount computations together with diff update to solve both lost focus and performance issues.

--- main/src/hbOrderSpreadsheetController.js
+++ main/src/hbOrderSpreadsheetController.js
@@ -24,11 +24,15 @@
 
 				$log.debug(">>>> HbOrderSpreadsheetController...");				
 				
-				$scope.GROSS_AMOUNT_TOTAL = "TOTAL_GROSS";
 				$scope.MANUAL_AMOUNT = "MANUAL_AMOUNT";
+				$scope.GROSS_AMOUNT_TOTAL = "TOTAL_GROSS";
+				$scope.REDUCTION_RATE = "REDUCTION_RATE";
+				$scope.DISCOUNT_RATE = "DISCOUNT_RATE";
+				$scope.ROUNDING_AMOUNT = "ROUNDING_AMOUNT";
+				$scope.VAT_RATE = "VAT_RATE";
+				$scope.NET_AMOUNT_TOTAL = "TOTAL_NET";
 				
 				$scope.numericOnlyRegexp = HB_REGEXP.NUMERIC_POS_ONLY;
-				
 				
 				/**
 				 * Reference to hb-single-select ng-model sibling directive controller
@@ -46,10 +50,9 @@
 				/**
 				 * Listener defining and updating $scope.hasManualOrderLine boolean value
 				 */
-				$scope.$watch('ngModelCtrl.$modelValue', function() {
-					
-					if ($scope.ngModelCtrl !== null && $scope.ngModelCtrl.$modelValue !== null) {
-						$log.debug(">>>> $modelValue CHANGED...");
+				$scope.$watch('ngModelCtrl.$modelValue.CARACTERISTIQUE.FRACTION.L', function() {
+										
+					if ($scope.ngModelCtrl !== null && $scope.ngModelCtrl.$modelValue !== null && $scope.ngModelCtrl.$modelValue.CARACTERISTIQUE.FRACTION.L) {
 						// Encapsulate two level deep underscorejs _.find 
 						$scope.hasManualOrderLine = _.find( 
 								// Loop on array of Ls (lines)
@@ -60,19 +63,8 @@
 									return ( manualAmtCell !== undefined) 
 								}
 							);
-					} else { 
-						// Nada
-					};
-				}, true);
-				
-				
-				/**
-				 * Listener defining and updating $scope.hasManualOrderLine boolean value
-				 */
-				$scope.$watch('ngModelCtrl.$modelValue.CARACTERISTIQUE.FRACTION.L', function() {
-										
-					if ($scope.ngModelCtrl !== null && $scope.ngModelCtrl.$modelValue !== null && $scope.ngModelCtrl.$modelValue.CARACTERISTIQUE.FRACTION.L) {
-						$log.debug(">>>> $modelValue.CARACTERISTIQUE.FRACTION.L CHANGED...");
+						
+						$scope.updateOrderLines($scope.elfinForm.$valid);
 					} else { 
 						// Nada
 					};
@@ -83,7 +75,7 @@
 				 * Boolean expression to make order line value conditionally editable
 				 */
 				$scope.lineValueEditable = function(line) {
-					return (line.C[0].VALUE === $scope.MANUAL_AMOUNT) || ((line.C[0].VALUE === $scope.GROSS_AMOUNT_TOTAL) && !$scope.hasManualOrderLine )
+					return (line.C[0].VALUE === $scope.MANUAL_AMOUNT) || (line.C[0].VALUE === $scope.ROUNDING_AMOUNT) || ((line.C[0].VALUE === $scope.GROSS_AMOUNT_TOTAL) && !$scope.hasManualOrderLine )
 				};
 				
 				
@@ -97,7 +89,8 @@
 						
 		        		restGeoxml.all("orders/compute/order-lines").post($scope.ngModelCtrl.$modelValue.CARACTERISTIQUE).then( 
 		               			function(updatedCaracteristique) {
-		               				$scope.ngModelCtrl.$modelValue.CARACTERISTIQUE = updatedCaracteristique;
+		               				//$scope.ngModelCtrl.$modelValue.CARACTERISTIQUE = updatedCaracteristique;
+		               				diffUpdate($scope.ngModelCtrl.$modelValue.CARACTERISTIQUE, updatedCaracteristique);
 		               				// Notify view of model update.
 		               				$scope.ngModelCtrl.$render();		               				
 		               				$scope.elfinForm.$setDirty();
@@ -113,15 +106,45 @@
 					}
 				};				
 			
-
-				//$scope.lineOrderTypeChoices = hbUtil.buildArrayFromCatalogueDefault("%|val|sum");				
+				/**
+				 * Update localCar by reference for computed values different from newCar
+				 */
+				var ORDER_LINE_TYPE_POS = 1;
+				var ORDER_LINE_PARAM_POS = 3;
+				var ORDER_LINE_AMOUNT_POS = 5;
 				
+				var diffUpdate = function (localCar, newCar) {
+					
+					for (var i = 0; i < localCar.FRACTION.L.length; i++) {
+						var currentLine = localCar.FRACTION.L[i];
+						var localLineTypeCell = hbUtil.getCByPos(currentLine.C, ORDER_LINE_TYPE_POS);
+						var localLineParamCell = hbUtil.getCByPos(currentLine.C, ORDER_LINE_PARAM_POS);
+						var localLineAmountCell = hbUtil.getCByPos(currentLine.C, ORDER_LINE_AMOUNT_POS);
+						
+						var newLine = newCar.FRACTION.L[i];
+						var newLineTypeCell = hbUtil.getCByPos(newLine.C, ORDER_LINE_TYPE_POS);
+						var newLineParamCell = hbUtil.getCByPos(newLine.C, ORDER_LINE_PARAM_POS);
+						var newLineAmountCell = hbUtil.getCByPos(newLine.C, ORDER_LINE_AMOUNT_POS);						
+						
+						// Gross amount is only computed if there is at least one manual order line
+						if (
+								localLineTypeCell.VALUE === $scope.GROSS_AMOUNT_TOTAL && $scope.hasManualOrderLine ||
+								localLineTypeCell.VALUE === $scope.REDUCTION_RATE ||
+								localLineTypeCell.VALUE === $scope.DISCOUNT_RATE ||
+								localLineTypeCell.VALUE === $scope.VAT_RATE ||
+								localLineTypeCell.VALUE === $scope.NET_AMOUNT_TOTAL 
+						) {
+							localLineAmountCell.VALUE = newLineAmountCell.VALUE;
+						}
+						
+					}
+				}
 				
 				/**
 				 * Adds a `manual entry` order line
 				 */
 				$scope.addLine = function (index, formValid) {
-					$log.debug("addLine("+index+")");
+
 					var L = {
 						          "C" : [ {
 						            "POS" : 1,
