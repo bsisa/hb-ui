b30fe99648abce190bc66f32e37dccb9992cc0ae
refond <refon@pobox.com>
2016-04-22 12:25:54 +0200

    Comment image overlay test for WIP. GIS funct. dev.

--- main/src/hbMapController.js
+++ main/src/hbMapController.js
@@ -22,49 +22,52 @@
                             name: 'Standard',
                             url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                             type: 'xyz'
-                        },
-// www.toolserver.org moved and could not find resource anymore.
-//                        black_white: {
-//                            name: 'Noir et Blanc',
-//                            url: 'http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png',
-//                            type: 'xyz'
-//                        },
-//                        hot: {
-//                            name: 'HOT',
-//                            url: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-//                            type: 'xyz'
-//                        },
-//                        transport: {
-//                            name: 'Transport',
-//                            url: 'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
-//                            type: 'xyz'
-//                        },
-//                        landscape: {
-//                            name: 'Paysage',
-//                            url: 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
-//                            type: 'xyz'
-//                        },
-//                        grayscale: {
-//                            name: 'Routes - Gris',
-//                            url: 'http://openmapsurfer.uni-hd.de/tiles/roadsg/x={x}&y={y}&z={z}',
-//                            type: 'xyz'
-//                        },
-//                        watercoloe: {
-//                            name: 'Aquarelle',
-//                            url: 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
-//                            type: 'xyz'
-//                        },
-                        gespatri: {
-                            name: 'Gespatri',
-                            type: 'imageOverlay',
-                            url: '/assets/images/abribus.jpeg',
-                            bounds: [[46.992737010038404, 6.931471483187033], [46.99302251894073, 6.931947239697114]],
-                            //{"_southWest":{"lat":46.992737010038404,"lng":6.931471483187033},"_northEast":{"lat":46.99302251894073,"lng":6.931947239697114}}
-                            layerParams: {
-                              noWrap: true,
-                              attribution: 'Custom map <a href="http://www.bsisa.ch/">by BSI SA</a>'
-                            }
-                        }                        
+                        }
+                // Image overlay test (WIP. GIS funct. dev.)
+//                		,
+//                        gespatri: {
+//                            name: 'Gespatri',
+//                            type: 'imageOverlay',
+//                            url: '/assets/images/abribus.jpeg',
+//                            bounds: [[46.992737010038404, 6.931471483187033], [46.99302251894073, 6.931947239697114]],
+//                            //{"_southWest":{"lat":46.992737010038404,"lng":6.931471483187033},"_northEast":{"lat":46.99302251894073,"lng":6.931947239697114}}
+//                            layerParams: {
+//                              noWrap: true,
+//                              attribution: 'Custom map <a href="http://www.bsisa.ch/">by BSI SA</a>'
+//                            }
+//                        }
+                
+                     // www.toolserver.org moved and could not find resource anymore.
+//                      black_white: {
+//                          name: 'Noir et Blanc',
+//                          url: 'http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png',
+//                          type: 'xyz'
+//                      },
+//                      hot: {
+//                          name: 'HOT',
+//                          url: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+//                          type: 'xyz'
+//                      },
+//                      transport: {
+//                          name: 'Transport',
+//                          url: 'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
+//                          type: 'xyz'
+//                      },
+//                      landscape: {
+//                          name: 'Paysage',
+//                          url: 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
+//                          type: 'xyz'
+//                      },
+//                      grayscale: {
+//                          name: 'Routes - Gris',
+//                          url: 'http://openmapsurfer.uni-hd.de/tiles/roadsg/x={x}&y={y}&z={z}',
+//                          type: 'xyz'
+//                      },
+//                      watercoloe: {
+//                          name: 'Aquarelle',
+//                          url: 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
+//                          type: 'xyz'
+//                      },                        
 
                     },
                     overlays: {
