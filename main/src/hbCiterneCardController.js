dbaf8b389ab7b59d60fdf421d05cf7ddc4f2c132
refond <refon@pobox.com>
2016-12-29 17:34:39 +0100

    Link per ID_G/CLASSE/Id (building list still uses OBJECTIF, ORIGINE)

--- main/src/hbCiterneCardController.js
+++ main/src/hbCiterneCardController.js
@@ -18,44 +18,37 @@
 
 						//$log.debug("    >>>> Using HbCiterneCardController ");
 						
-						// Used to provide navigation link back to IMMEUBLE link to this CITERNE
-						$scope.immeubleRef = null;			 			
-			 			
-			        	// Navigate to IMMEUBLE linked to the current CITERNE
-			        	$scope.viewImmeuble = function() {
-			        		$location.path('/elfin/'+$scope.immeubleRef.ID_G+'/IMMEUBLE/' + $scope.immeubleRef.Id);	
-			        	};
-			 			
-			        	
-				    	$scope.$watchCollection('[elfin.IDENTIFIANT.OBJECTIF,elfin.IDENTIFIANT.ORIGINE]', function(newValues, oldValues) {							    		
+						/**
+						 * Perform current elfin updates related to parent reference
+						 */
+						var updateElfinParentRef = function(parentElfin) {
+		 					// Set SOURCE to selected building reference triplet ID_G/CLASSE/Id
+		 					$scope.elfin.SOURCE = hbUtil.getStandardSourceURI(parentElfin);
+		 					// Update OBJECTIF information to preserve historic behaviour. With SOURCE triple it creates data redundancy. 
+	 						$scope.elfin.IDENTIFIANT.OBJECTIF = parentElfin.IDENTIFIANT.OBJECTIF;
+	 						// No construction 
+	 						$scope.elfin.IDENTIFIANT.ORIGINE = parentElfin.IDENTIFIANT.NOM;
+	 						// Notify and allow end-user to save parent selection modification.
+	 						$scope.elfinForm.$setDirty();
+						};
+						
+						/**
+						 * Loads elfin parent in scope. `updateElfinParentRef` need to be called from 
+						 * within the `GeoxmlService.getElfin` promise thus the ugly `forUpdate` pattern.
+						 */
+						var getParentElfinInScope = function(idg, id, forUpdate) {
 
-				    		//$log.debug("$watchCollection for OBJECTIF, ORIGINE : " + oldValues[0] + ", " + oldValues[1] + " => " + newValues[0] + ", " + newValues[1]);
-				    		
-				    		if ($scope.elfin!=null && $attrs.hbMode != "create") {
-					    		var saiNb = $scope.elfin.IDENTIFIANT.OBJECTIF;
-								var buildingNb = $scope.elfin.IDENTIFIANT.ORIGINE;
-								xpathForImmeubleLinkedToCurrentCiterne = "//ELFIN[IDENTIFIANT/OBJECTIF='"+saiNb+"' and IDENTIFIANT/NOM='"+buildingNb+"']";
-								hbQueryService.getImmeubles(xpathForImmeubleLinkedToCurrentCiterne)
-									.then(function(immeubles) {
-										// Expected case
-										if (immeubles.length === 1) {
-											var immeuble = immeubles[0];
-											$scope.immeubleRef = { "Id" : immeuble.Id,  "ID_G" : immeuble.ID_G , "address": immeuble.IDENTIFIANT.ALIAS, "owner": immeuble.PARTENAIRE.PROPRIETAIRE.NOM};
-										} else if (immeubles.length < 1) { 
-											var message = "L'IMMEUBLE correspondant à la CITERNE courrante pour le no de construction "+ buildingNb +" et no SAI "+ saiNb +" n'a pas pu être trouvé dans la base de donnée!";
-								            hbAlertMessages.addAlert("danger",message);
-										} else {
-											var message = "Plusieurs IMMEUBLE correspondent à la CITERNE courrante pour le no de construction: " + buildingNb +" et no SAI "+ saiNb +". Le résultat devrait être unique.";
-								            hbAlertMessages.addAlert("danger",message);
-										}
-									},
-									function(response) {
-										var message = "Le chargement de l'IMMEUBLE lié à la CITERNE courrante pour le no de construction "+ buildingNb +" et no SAI "+ saiNb + " a échoué (statut de retour: "+ response.status+ ")";
-							            hbAlertMessages.addAlert("danger",message);
-									});										
-				    			
-				    		}
-				    	}, true);
+		 					// Get full parent elfin to allow details display in current context.
+		 					GeoxmlService.getElfin(idg, id).get().then(function(elfin) {
+		 						$scope.parentElfin = elfin;
+		 						if (forUpdate) {
+		 							updateElfinParentRef($scope.parentElfin);
+		 						}
+				            }, function() {
+				            	var message = "L'objet sélectionné ID_G = " + parentSelection.ID_G + ", Id = " + parentSelection.Id +" n'a pu être obtenu. Un ajustement des configurations peut être nécessaire. Veuillez contacter votre administrateur système. (statut de retour: "+ response.status+ ")";
+					            hbAlertMessages.addAlert("warning",message);
+				            });									
+						};
 			        	
 			 			
 						// Get available roles dynamically from HB5 catalogue
@@ -91,17 +84,43 @@
 									});
 					            
 								if ( $attrs.hbMode === "create" ) {
+									
+									if ($routeParams.id && $routeParams.idg) {
+					 					var forUpdate = true;
+					 					getParentElfinInScope($routeParams.idg, $routeParams.id, forUpdate);
+									}									
 									// Reset default xxx from catalogue
-									$scope.elfin.IDENTIFIANT.OBJECTIF = $routeParams.sai;
-									$scope.elfin.IDENTIFIANT.ORIGINE = $routeParams.nocons;
 									$scope.elfin.GROUPE = "";
 									$scope.elfin.CARACTERISTIQUE['CAR3'].VALEUR = "";
 									$scope.elfin.CARACTERISTIQUE['CAR4'].VALEUR = "";
 									$scope.elfin.CARACTERISTIQUE['CAR5'].VALEUR = "";
 									
 								} else {
-									// Do nothing
-									
+
+									if ($routeParams.selectionImmeuble) {
+										
+					 					$log.debug("$routeParams.selectionImmeuble = " + $routeParams.selectionImmeuble);
+					 					// Parse building selection string
+					 					var parentSelectionStrSplit = $routeParams.selectionImmeuble.split('/');
+					 					var parentSelection = {
+					 						"ID_G" : parentSelectionStrSplit[0],
+					 						"CLASSE" : parentSelectionStrSplit[1],
+					 						"Id" : parentSelectionStrSplit[2]
+					 					}
+					 					var forUpdate = true;
+					 					getParentElfinInScope(parentSelection.ID_G, parentSelection.Id, forUpdate);
+					 					
+					 				} else {									
+										// Load parent reference
+					 					var sourceStrSplit = $scope.elfin.SOURCE.split('/');
+					 					var source = {
+					 						"ID_G" : sourceStrSplit[0],
+					 						"CLASSE" : sourceStrSplit[1],
+					 						"Id" : sourceStrSplit[2]
+					 					}
+										var forUpdate = false;
+					 					getParentElfinInScope(source.ID_G, source.Id, forUpdate);
+					 				}									
 								}					            
 								
 								
