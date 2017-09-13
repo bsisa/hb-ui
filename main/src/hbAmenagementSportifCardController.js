b90d0a42282cca39aba54ab121298e0a1958f822
Guy de Pourtales <guy@skioo.com>
2017-09-13 07:40:24 +0200

    Fixed issue with AMENAGEMENTS SPORTIFS prestation list

--- main/src/hbAmenagementSportifCardController.js
+++ main/src/hbAmenagementSportifCardController.js
@@ -281,7 +281,7 @@
                             // TODO: constatsCollectionId must come from server configuration resource.
                             //$log.debug("TODO: HbAMENAGEMENT_SPORTIFCardController: prestationCollectionId must come from server configuration resource.");
                             // DONE: added restriction on PROPRIETAIRE, CLASSE. Strict restriction on OBJECTIF, starts-with is not correct in all cases.
-                            var xpathForPrestations = "//ELFIN[substring-before(IDENTIFIANT/OBJECTIF,'.')='" + $scope.elfin.IDENTIFIANT.OBJECTIF + "' and PARTENAIRE/PROPRIETAIRE/@NOM='" + $scope.elfin.PARTENAIRE.PROPRIETAIRE.NOM + "' and @CLASSE='PRESTATION']";
+                            var xpathForPrestations = "//ELFIN[starts-with(IDENTIFIANT/OBJECTIF, '" + $scope.elfin.IDENTIFIANT.OBJECTIF + "') and PARTENAIRE/PROPRIETAIRE/@NOM='" + $scope.elfin.PARTENAIRE.PROPRIETAIRE.NOM + "' and @CLASSE='PRESTATION']";
                             // TODO: evaluate replacing the above by the following.
                             //$log.debug(">>>> amenagement_sportif: xpathForPrestations = " + xpathForPrestations)
                             //var xpathForPrestations = "//ELFIN[substring-before(IDENTIFIANT/OBJECTIF,'.')='"+$scope.elfin.IDENTIFIANT.OBJECTIF+"' and PARTENAIRE/PROPRIETAIRE/@Id='"+$scope.elfin.PARTENAIRE.PROPRIETAIRE.Id+"' and PARTENAIRE/PROPRIETAIRE/@ID_G='"+$scope.elfin.PARTENAIRE.PROPRIETAIRE.ID_G+"' and @CLASSE='PRESTATION']";
