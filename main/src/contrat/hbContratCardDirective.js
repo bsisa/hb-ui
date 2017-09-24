bcc9e53955aaf4b10ca24618c117f4301998a74a
Guy de Pourtales <guy@skioo.com>
2017-09-24 11:16:04 +0200

    Refactor contrat mask Added constraint to fields as numbers

--- main/src/contrat/hbContratCardDirective.js
+++ main/src/contrat/hbContratCardDirective.js
@@ -10,7 +10,7 @@
     	return {
 		    require: '^hbCardContainer',
 			restrict: 'A',
-		    templateUrl : "/assets/views/hbContratCard.html",
+		    templateUrl : "/assets/views-compiled/hbContratCard.html",
 			controller: 'HbContratCardController'
 		};
 	
